612deb23e4cea16ca5a758e26a81809630a93e10
rurusasu <rurusasu153@gmail.com>
2020-09-26 20:40:09 +0900

    chocolateyを自動インストールするプログラムを構築中

---
+++ Chocolatey/start-process.ps1
@@ -0,0 +1,9 @@
+﻿# 管理者権限で実行したいファイルのパス
+$file = (Convert-Path .\choco_install.ps1)
+Write-Output $file
+
+# PowerShellを管理者権限で実行
+Start-Process -FilePath powershell -ArgumentList $file -Verb runas 
+
+# スクリプト実行後、自動的にPowerShellが閉じないようにする
+pause
