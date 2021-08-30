15fadcb1b7f5e57831122ae1afa2d5c7437133a7
rurusasu <rurusasu153@gmail.com>
2021-08-30 14:49:14 +0900

    cuda インストーラ内部をNVIDIA準拠に変更

--- wsl/install-cuda-on-wsl-orig.sh
+++ wsl/install-cuda-on-wsl-orig.sh
@@ -5,7 +5,7 @@
 #
 #install-cuda-on-wsl.sh
 #WSL2のUbuntu側で行うCUDA on WSLのセットアップスクリプト
-#最終更新：2021/08/30
+#最終更新：2021/07/17
 #https://astherier.com/blog/2021/07/windows11-cuda-on-wsl2-setup/
 
 #CUDAリポジトリ名(DISTRO)とインストールするパッケージ名(APT_INSTALL)を指定してください。
@@ -17,13 +17,10 @@ export APT_INSTALL=cuda
 #export APT_INSTALL=cuda-toolkit-11-4
 
 #CUDAリポジトリの登録
-#Origファイルより更新(NVIDIAのサイト準拠に変更)
-#https://docs.nvidia.com/cuda/wsl-user-guide/index.html
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 wget https://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/x86_64/cuda-${DISTRO}.pin
-sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
-sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-4-local/7fa2af80.pub
+sudo mv cuda-${DISTRO}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
 #CUDAツールキットのインストール
 sudo apt update && sudo apt -y upgrade
